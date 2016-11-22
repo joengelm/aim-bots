96ecd7d01ed2319ae6f6cd4d48f433f63c8dff80
joengelm <joseph_engelman@brown.edu>
2016-11-21 23:32:59 -0500

    Working version with Heroku files added

---
+++ app.py
@@ -0,0 +1,40 @@
+from cleverbot import Cleverbot
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+id_to_bot = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/message', methods=['POST'])
+def get_message_from_bot():
+    bot_id = request.form['id']
+    prompt = request.form['prompt']
+
+    print bot_id + ' has received prompt: ' + prompt
+
+    message = get_message(bot_id, prompt)
+    return jsonify(message=message)
+
+def create_bot_with_id(bot_id):
+    bot = Cleverbot()
+    id_to_bot[bot_id] = bot
+    return bot
+
+def get_message(bot_id, prompt='Hello.'):
+    if bot_id not in id_to_bot:
+        create_bot_with_id(bot_id)
+
+    bot = id_to_bot[bot_id]
+    msg = bot.ask(prompt).encode('utf-8')
+
+    print bot_id + ': ' + msg
+
+    return msg
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- static/js/aim.js
+++ static/js/aim.js
@@ -1,9 +1,120 @@
-const addMessage = (name, message, color, voice) => {
-	$('#chatbox').append('<p class="chattext"><span style="color:' + color + '">' + name + '</span>: ' + message + '</p>');	
-	console.log('appended ' + message + ' for ' + name);
-	responsiveVoice.speak(message, voice);
+const adjectives = ['yellow', 'savvy', 'wild', 'crazy', 'dead', 'rich', 'poor', 'happy', 'musical', 'purring', 'bad', 'good', 'old', 'yung', 'mean', 'funny', 'b4d', 'gr33n', 'blue', 'red', 'ded', '', '', '', '', '', '', '', '', '', ''];
+const nouns = ['joey', 'sushi', 'mikey', 'serg', 'felip', 'cat', 'kitten', 'burrito', 'fangurl', 'fanboi', 'baseb4llr', 'erikaa', 'emily', 'willy', 'zombie', 'sock', 'witch', 'wizard', 'boy', 'girl', 'woman', 'man', 'elf', 'hunter', '', '', '', '', '', '', '', ''];
+const colors = ['blue', 'red', 'green', 'purple', 'violet', 'brown'];
+const voices = ['UK English Male', 'UK English Female', 'US English Female'];
+
+let scrolled = false;
+const updateScroll = () => {
+    if (!scrolled) {
+        const element = document.getElementById("chatbox");
+        element.scrollTop = element.scrollHeight;
+    }
+}
+
+$("#yourDivID").on('scroll', () => {
+    scrolled = true;
+});
+
+const addMessage = (bot, message) => {
+	$('#chatbox').append('<p class="chattext"><span style="color:' + bot.color + '">' + bot.name + '</span>: ' + message + '</p>');	
+	console.log('appended ' + message + ' for ' + bot.name);
+	setTimeout(updateScroll, 100);
+	responsiveVoice.speak(message, bot.voice);
 };
 
-setTimeout(() => addMessage('joey2000', 'Hi.', 'red', 'US English Male'), 500);
-setTimeout(() => addMessage('erikaaat', 'Hello.', 'blue', 'UK English Female'), 2000);
-setTimeout(() => addMessage('joey2000', 'How are you?', 'red', 'US English Male'), 4000);
+const responseFromBot = (bot, prompt, callback) => {
+	const params = {
+		id: bot.id,
+		prompt: prompt
+	};
+	$.post('/message', params, (response) => {
+		const message = response['message'];
+		console.log('prompt: ' + prompt);
+		setTimeout(() => { callback(message) }, 1000 + prompt.length * 70);
+	});
+};
+
+const chat = (bot1, bot2, prompt) => {
+	addMessage(bot1, prompt);
+
+	responseFromBot(bot2, prompt, (response) => {
+		addMessage(bot2, response);
+
+		responseFromBot(bot1, response, (nextPrompt) => {
+			chat(bot1, bot2, nextPrompt);
+		});
+	});
+};
+
+const emptyBot = () => {
+	return {
+		name: '',
+		color: '',
+		voice: ''
+	};
+};
+
+const choose = (list) => {
+	return list[Math.floor(Math.random() * list.length)];
+};
+
+const getId = () => {
+	const letter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+	return letter + Date.now().toString();
+};
+
+const getName = () => {
+	const adj = choose(adjectives);
+	const noun = choose(nouns);
+	let number = '';
+	if (Math.random() > 0.3) {
+		number = Math.floor(Math.random() * 10000).toString();
+	}
+
+	const name = adj + noun + number;
+	if (name.length < 5) {
+		return getName();
+	}
+	return name;
+};
+
+const getColor = () => {
+	return choose(colors);
+};
+
+const getVoice = () => {
+	return choose(voices);
+};
+
+const makeDistinctBot = (otherBot) => {
+	let name = otherBot.name;
+	while (name === otherBot.name) {
+		name = getName();
+	}
+
+	let color = otherBot.color;
+	while (color === otherBot.color) {
+		color = getColor();
+	}
+
+	let voice = otherBot.voice;
+	while (voice === otherBot.voice) {
+		voice = getVoice();
+	}
+
+	return {
+		id: getId(),
+		name: name,
+		color: color,
+		voice: voice
+	};
+};
+
+const main = (prompt) => {
+	bot1 = makeDistinctBot(emptyBot());
+	bot2 = makeDistinctBot(bot1);
+
+	chat(bot1, bot2, prompt);
+};
+
+setTimeout(() => { main('Hello.') }, 2000);
